170c11bdf4ff2934f7047374d2abb787a7edab57
Allen Hurlbert <hurlbert@bio.unc.edu>
2015-11-23 13:45:49 -0500

    simple script for calculating basic summary stats on database

---
+++ database_summary.R
@@ -0,0 +1,28 @@
+# Data summaries of the Avian Diet Database
+
+setwd('z:/git/dietdatabase')
+
+#################
+simpleCap <- function(x) {
+  s <- strsplit(x, " ")[[1]]
+  paste(toupper(substring(s, 1,1)), substring(s, 2), sep="", collapse=" ")
+}
+
+
+
+diet = read.table('aviandietdatabase.txt', header=T, sep = '\t', quote = '\"',
+                  fill=T, stringsAsFactors = F)
+
+# Replace typos
+diet$Common_Name = sapply(diet$Common_Name, simpleCap)
+diet$Common_Name = gsub('Western Wood-Pewee', 'Western Wood-pewee', diet$Common_Name)
+
+
+dietSummary = function(diet) {
+  species = unique(diet$Common_Name)
+  numSpecies = length(species)
+  numStudies = length(unique(diet$Source))
+  numRecords = nrow(diet)
+  recordsPerSpecies = data.frame(table(diet$Common_Name))
+  return(list(numRecords, numSpecies, numStudies, recordsPerSpecies))
+}
