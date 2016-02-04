51666d00189844078ac11f415bff59c83a32bf5f
Allen Hurlbert <hurlbert@bio.unc.edu>
2016-02-04 15:25:04 -0500

    modified dietdatabase summary fn to give #spp per fam w/o data

--- database_summary.R
+++ database_summary.R
@@ -12,22 +12,31 @@ simpleCap <- function(x) {
 
 diet = read.table('aviandietdatabase.txt', header=T, sep = '\t', quote = '\"',
                   fill=T, stringsAsFactors = F)
+refs = read.table('NA_avian_diet_refs.txt', header=T, sep = '\t', quote = '\"',
+                  fill=T, stringsAsFactors = F)
+
 
 # Replace typos
 diet$Common_Name = sapply(diet$Common_Name, simpleCap)
 diet$Common_Name = gsub('Western Wood-Pewee', 'Western Wood-pewee', diet$Common_Name)
 
 
-dietSummary = function(diet) {
+dietSummary = function(diet, refs) {
   species = unique(diet[, c('Common_Name', 'Family')])
+  allspecies = unique(refs[, c('common_name', 'family')])
   numSpecies = nrow(species)
   numStudies = length(unique(diet$Source))
   numRecords = nrow(diet)
   recordsPerSpecies = data.frame(table(diet$Common_Name))
   spCountByFamily = data.frame(table(species$Family))
+  noDataSpecies = subset(allspecies, !common_name %in% species$Common_Name)
+  noDataSpCountByFamily = data.frame(table(noDataSpecies$family))
+  spCountByFamily2 = merge(spCountByFamily, noDataSpCountByFamily, by = "Var1", all.x = T)
+  names(spCountByFamily2) = c('Family', 'SpeciesWithData', 'WithoutData')
+  spCountByFamily2$WithoutData[is.na(spCountByFamily2$WithoutData)] = 0
   return(list(numRecords=numRecords,
               numSpecies=numSpecies, 
               numStudies=numStudies, 
               recordsPerSpecies=recordsPerSpecies,
-              speciesPerFamily = spCountByFamily))
+              speciesPerFamily = spCountByFamily2))
 }
