7a6bc0e085e57401f08e7aad27e6b15346c641c4
Allen Hurlbert <hurlbert@bio.unc.edu>
2016-12-09 11:51:45 -0500

    fixed Bird Sample Size/Item Sample Size errors in Newton 1967, errant Beg Year obs in Beal 1912

--- database_error_checking.R
+++ database_error_checking.R
@@ -19,27 +19,45 @@ outlier = function(field, min, max) {
 
 
 check = function(diet) {
-  long = outlier(diet$Longitude_dd, -180, 180)
-  
-  lat = outlier(diet$Latitude_dd, -180, 180)
-  
-  alt_min = outlier(diet$Altitude_min_m, -100, 10000)
-  
-  alt_mean = outlier(diet$Altitude_mean_m, -100, 10000)
-  
-  alt_max = outlier(diet$Altitude_max_m, -100, 10000)
-  
-  region = count(diet, Location_Region) 
-  
-  location = count(diet, Location_Specific)
-  
-  habitat = count(diet, Habitat_type)
-  
-  mon_beg = outlier(diet$Observation_Month_Begin, 0, 12)
-  
-  mon_end = outlier(diet$Observation_Month_End, 0, 12)
-  
-  year_beg = outlier(diet$Observation_Year_Begin, 1800, 2017)
-  
-  year_end = outlier(diet$Observation_Year_End, 0, 2017)
+  out = list(
+    long = outlier(diet$Longitude_dd, -180, 180),
+    
+    lat = outlier(diet$Latitude_dd, -180, 180),
+    
+    alt_min = outlier(diet$Altitude_min_m, -100, 10000),
+    
+    alt_mean = outlier(diet$Altitude_mean_m, -100, 10000),
+    
+    alt_max = outlier(diet$Altitude_max_m, -100, 10000),
+    
+    mon_beg = outlier(diet$Observation_Month_Begin, 0, 12),
+    
+    mon_end = outlier(diet$Observation_Month_End, 0, 12),
+    
+    year_beg = outlier(diet$Observation_Year_Begin, 1800, 2017),
+    
+    year_end = outlier(diet$Observation_Year_End, 0, 2017),
+    
+    diet_wt = outlier(diet$Fraction_Diet_By_Wt_or_Vol, 0, 1),
+    
+    diet_items = outlier(diet$Fraction_Diet_By_Items, 0, 1),
+    
+    diet_occ = outlier(diet$Fraction_Occurrence, 0, 1),
+    
+    diet_unk = outlier(diet$Fraction_Diet_Unspecified, 0, 1),
+    
+    item_sampsize = outlier(diet$Item_Sample_Size, 0, 10000),
+    
+    bird_sampsize = outlier(diet$Bird_Sample_Size, 0, 1000)
+    
+    #season = count(diet, Observation_Season),
+    
+    #region = count(diet, Location_Region), 
+    
+    #location = count(diet, Location_Specific),
+    
+    #habitat = count(diet, Habitat_type)
+  )
+    
+  return(out)
 }
