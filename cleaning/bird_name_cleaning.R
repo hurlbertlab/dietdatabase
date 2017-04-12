173acf347d25c1374c81db31f7f76b77b37acf2f
Allen Hurlbert <hurlbert@bio.unc.edu>
2017-04-12 13:17:37 -0400

    all bird names (sci & common) now in compliance with eBird 2016 checklist

---
+++ cleaning/bird_name_cleaning.R
@@ -0,0 +1,30 @@
+# Cleaning bird taxonomic names to be in line with the eBird Clements 2016 checklist.
+
+library(dplyr)
+library(stringr)
+
+#-------------------------------------------------------------------------------------------                               
+# Read in diet database and eBird taxonomy table
+diet = read.table('aviandietdatabase.txt', header=T, sep = '\t', quote = '\"',
+                  fill=T, stringsAsFactors = F)
+
+# Make sure to grab the most recent eBird table in the directory
+taxfiles = file.info(list.files()[grep('eBird', list.files())])
+taxfiles$name = row.names(taxfiles)
+tax = read.table(taxfiles$name[taxfiles$mtime == max(taxfiles$mtime)], header = T,
+                 sep = ',', quote = '\"', stringsAsFactors = F)
+orders = unique(tax[, c('ORDER', 'FAMILY')])
+orders$Family = word(orders$FAMILY, 1)
+orders = filter(orders, FAMILY != "" & ORDER != "") %>%
+  select(ORDER, Family)
+
+
+db_spp = select(diet, Common_Name, Scientific_Name, Family) %>% unique()
+
+# Find common names or scientific names that are unmatched with the eBird checklist
+unmatched = anti_join(db_spp, tax, by = c("Common_Name" = "PRIMARY_COM_NAME"))
+
+unmatchedsci = anti_join(db_spp, tax, by = c("Scientific_Name" = "SCI_NAME"))
+
+unmatched2 = anti_join(db_spp, tax, by = c("Common_Name" = "PRIMARY_COM_NAME", "Scientific_Name" = "SCI_NAME"))
+
