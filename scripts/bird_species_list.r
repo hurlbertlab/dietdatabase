82c25ea3b73abb67f0154f10eff603e636dcadef
Allen Hurlbert <hurlbert@bio.unc.edu>
2017-04-13 15:54:14 -0400

    code for tabulating expected and actual species in db

--- scripts/bird_species_list.r
+++ scripts/bird_species_list.r
@@ -1,18 +1,53 @@
-# 
+# Checklist of birds of the United States and Canada obtained from Avibase here:
+# https://avibase.bsc-eoc.org/checklist.jsp?lang=EN&p2=1&list=clements&synlang=&region=NA1&version=text&lifelist=&highlight=0
+
+#Description: All 50 US States, Canada, St-Pierre & Miquelon
+#Number of species: 1216
+#Number of endemics: 25
+#Number of breeding endemics: 1
+#Number of globally threatened species: 106
+#Number of extinct species: 24
+#Number of introduced species: 130
+
+# Read this file in, then weed out all species that are Extinct, Rare/Accidental, or Introduced.
+
 library(dplyr)
+library(stringr)
 
-# Read in files
-# AOU species list from BBS:
-spp = read.table('z:/lab/databases/bbs/species.txt', header=T, sep = '\t', quote = '')
+birdtax = read.table('eBird_Taxonomy_v2016_NorthAmerica.txt', header=T, sep = '\t', 
+                     quote = '', fill = T, stringsAsFactors = FALSE)
 
-# Species in reference list
-refs = read.table('NA_avian_diet_refs.txt', header=T, sep = '\t', quote = '\"',
-                  fill=T, stringsAsFactors = F)
-refsp = unique(refs[, c('common_name', 'family')])
+exclude = c("Introduced", "Extinct", "Extirpated", "Rare/Accidental")
 
-# Add lowercase name column to facilitate merging
-spp$lowsp = tolower(spp$Common.Name.English)
-refsp$lowsp = tolower(refsp$common_name)
+btax = filter(birdtax, !grepl(paste(exclude, collapse = "|"), Status))
 
-bbs_not_db = anti_join(spp, refsp, by = 'lowsp')
-db_not_bbs = anti_join(refsp, spp, by = 'lowsp')
+# Add a few species that are introduced but long enough ago to be considered part of the avifauna
+add = filter(birdtax, CommonName %in% c("Rock Pigeon", "House Sparrow", "Alder/Willow Flycatcher (Traill's Flycatcher)",
+                                        "Mute Swan", "Ring-necked Pheasant"))
+
+tax = rbind(btax, add)
+
+# Merge in Family and Order names from overall eBird 2016 checklist
+ebird = read.table('eBird_Taxonomy_v2016.csv', header = T,
+                 sep = ',', quote = '\"', stringsAsFactors = F)
+
+spplist = left_join(tax, ebird, by = c("CommonName" = "PRIMARY_COM_NAME")) %>%
+  mutate(Family = word(FAMILY, 1), list = 1) %>% 
+  select(CommonName, SciName, Family, ORDER, list)
+
+# Now compare to species in the Diet Database
+diet = read.table('AvianDietDatabase.txt', header=T, sep = '\t', quote = '', fill = T, stringsAsFactors = FALSE)
+
+dietsp = data.frame(CommonName = unique(diet$Common_Name), dietdb = 1)
+
+spplist2 = full_join(spplist, dietsp)
+
+spplist2$list[is.na(spplist2$list)] = 0
+spplist2$dietdb[is.na(spplist2$dietdb)] = 0
+
+
+famtotals = spplist2 %>% group_by(ORDER, Family) %>% 
+  summarize(TotalSp = sum(list), WithData = sum(dietdb)) %>%
+  mutate(WithoutData = TotalSp - WithData) %>%
+  data.frame()
+
