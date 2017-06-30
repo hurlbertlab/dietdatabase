1abcb7914f2e9983a1ddd3d88050e8e7e1eddfa2
Patrick Winner <patrick.m.winner@gmail.com>
2017-06-30 12:37:59 -0400

    minor tweaks

--- cleaning/compare_previous_db.R
+++ cleaning/compare_previous_db.R
@@ -1,11 +1,12 @@
 # Getting list of names that don't match between the current database and 
-# an old version.
+# an old version. (Apr 13 2017 seems to be last time cleaning was done)
 
 # Read in datasets
 diet = read.table('aviandietdatabase.txt', header=T, sep = '\t', quote = '\"',
                   fill=T, stringsAsFactors = F)
 
-olddiet = read.table('olddietdatabase.txt' ...)
+olddiet = read.table('olddietdatabase.txt', header=T, sep = '\t', quote = '\"',
+                     fill=T, stringsAsFactors = F)
 
 list = anti_join(diet, olddiet) %>% select(Common_Name) %>% distinct()
 
