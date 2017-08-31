a992a75459f27a1039e6a4da551cae97e2c95e86
Hurlbert <hurlbert@ad.unc.edu>
2017-08-30 22:38:46 -0400

    created qa_qc function for error checking

--- cleaning/database_error_checking.R
+++ cleaning/database_error_checking.R
@@ -7,32 +7,33 @@ library(taxize)
 source('scripts/database_summary_functions.R')
 
 
-diet = read.table('aviandietdatabase.txt', header=T, sep = '\t', quote = '\"',
-                  fill=T, stringsAsFactors = F)
+qa_qc = function(diet, write = FALSE, filename = NULL) {
+  
+  # Error checking -- numeric fields
+  outliers = outlierCheck(diet)
 
-# Error checking -- quantitative valeus
-# Checks for outliers that should be double-checked and/or fixed
-out = outlierCheck(diet)
+  # Error checking -- text fields
+  season = count(diet, Observation_Season) %>% arrange(desc(n)) %>% data.frame()
+  
+  region = count(diet, Location_Region) %>% arrange(desc(n)) %>% data.frame()
+  
+  location = count(diet, Location_Specific) %>% arrange(desc(n)) %>% data.frame()
+  
+  habitat = count(diet, Habitat_type) %>% arrange(desc(n)) %>% data.frame()
+  
+  taxonomy = count(diet, Taxonomy) %>% arrange(desc(n)) %>% data.frame()
+  
+  stage = count(diet, Prey_Stage) %>% arrange(desc(n)) %>% data.frame()
+  
+  part = count(diet, Prey_Part) %>% arrange(desc(n)) %>% data.frame()
+  
+  diettype = count(diet, Diet_Type) %>% arrange(desc(n)) %>% data.frame()
+  
+  output = list(outliers, season, region, location, habitat, 
+                taxonomy, stage, part, diettype)
+  
+  return(output)
+}
 
 
-# Error checking -- text fields
-# Checks for the 
-season = count(diet, Observation_Season) %>% arrange(desc(n)) %>% data.frame()
-
-region = count(diet, Location_Region) %>% arrange(desc(n)) %>% data.frame()
-
-location = count(diet, Location_Specific) %>% arrange(desc(n)) %>% data.frame()
-
-habitat = count(diet, Habitat_type) %>% arrange(desc(n)) %>% data.frame()
-
-taxonomy = count(diet, Taxonomy) %>% arrange(desc(n)) %>% data.frame()
-
-stage = count(diet, Prey_Stage) %>% arrange(desc(n)) %>% data.frame()
-
-part = count(diet, Prey_Part) %>% arrange(desc(n)) %>% data.frame()
-
-diettype = count(diet, Diet_Type) %>% arrange(desc(n)) %>% data.frame()
-
-
-# Finding a list of studies where habitat type is blank
-diet %>% filter(Habitat_type=="") %>% select(Source) %>% unique()
+# Taxonomic name cleaning
