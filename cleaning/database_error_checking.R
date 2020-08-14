330ccd688a9f5c9669670f0d9c1a470e70b32881
Allen Hurlbert <hurlbert@bio.unc.edu>
2020-08-14 16:31:19 -0400

    removing unused function

--- cleaning/database_error_checking.R
+++ cleaning/database_error_checking.R
@@ -68,31 +68,6 @@ simpleCap <- function(x) {
   paste(toupper(substring(s, 1,1)), substring(s, 2), sep="", collapse=" ")
 }
 
-# Function to remove unintended leading or trailing spaces from text fields
-
-LeadingAndTrailingSpaceRemover = function(dietdatabase) {
-  characterFields = which(sapply(dietdatabase, class) == "character")
-  
-  for (i in characterFields) {
-    for (j in 1:nrow(dietdatabase)) {
-      val = dietdatabase[j, i]
-      
-      if (!is.na(val)) {
-        #leading and trailing spaces
-        if (substring(val, 1, 1) == " " & substring(val, nchar(val), nchar(val)) == " ") {
-          dietdatabase[j, i] = substring(val, 2, nchar(val) - 1)
-          #leading spaces
-        } else if (substring(val, 1, 1) == " ") {
-          dietdatabase[j, i] = substring(val, 2, nchar(val))
-          #trailing spaces
-        } else if (substring(val, nchar(val), nchar(val)) == " ") {
-          dietdatabase[j, i] = substring(val, 1, nchar(val) - 1)
-        }  
-      }
-    }
-  }
-  return(dietdatabase)
-}
 
 
 
